93c6002ac2e014178c2e41311170b0f6f605d36d
abel <abelw34@gmail.com>
2022-07-27 02:18:16 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -3,15 +3,34 @@ import express from "express";
 import expenses from "./routes/post.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
-
 app.use("/api/v1/post", expenses);
+
+const PORT = process.env.PORT || 3000;
+
+//deploment
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("hey there API is running");
+  });
+}
+
+//deployment
 
 mongoose
   .connect(process.env.MONGO_URI)
